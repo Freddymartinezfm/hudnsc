d73f213aa38dcd6075abe900f8d9c093cd039478
freddy <freddymartinezfm@icloud.com>
2022-03-04 18:03:58 -0600

    login mfa click on search and search for keyword pending

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,7 +1,13 @@
+import com.sun.security.jgss.GSSUtil;
+import net.jodah.failsafe.Fallback;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.security.cert.TrustAnchor;
+import java.sql.Struct;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Dashboard {
@@ -19,70 +25,49 @@ public class Dashboard {
     }
 
 
-    public void login() throws InterruptedException {
+
+
+    public boolean login() throws InterruptedException {
         driver.get("https://hud-stage.hudnsc.org/evars/index.cfm?");
         Thread.sleep(500);
 
         WebElement login_btn = driver.findElement(By.id("main_dsp-login-btn"));
         Thread.sleep(500);
 
-        if (login_btn != null) login_btn.click();
+        if (login_btn == null){
+            return false;
+        }
+        login_btn.click();
         WebElement login_email_field = driver.findElement(By.id("loginEmail"));
-        Thread.sleep(1000);
+        Thread.sleep(500);
 
         login_email_field.sendKeys("freddy.martinez@hudnsc.org");
         WebElement login_password_field = driver.findElement(By.id("loginPassword"));
-        Thread.sleep(1000);
+        Thread.sleep(500);
 
         login_password_field.sendKeys("Tara!234");
         driver.findElement(By.id("loginSubmit")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
+
+        System.out.println("Successfully enter credentials");
+
+        System.out.println("Processing MFA");
+        boolean mfa_auth =  handle_multifactor_authentication();
+
+        if (!mfa_auth){
+            System.out.println("ERROR! Multi-factor authentication failed. Please try again.");
+        } else {
+            Thread.sleep(5000);
+            var requestors = driver.findElement(By.cssSelector("#navbar > ul > li:nth-child(3) > a"));
+            requestors.click();
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("//*[@id=\"navbar\"]/ul/li[3]/ul/a[3]/li")).click();
+
+        }
+        return true;
 
 
-        // open pending in diff tabs
-        var duo_iframe = driver.findElement(By.id("duo_iframe"));
-        driver.switchTo().frame(duo_iframe);
-        System.out.println(driver.getTitle());
-
-        Thread.sleep(1000);
-        driver.findElement(By.id("passcode")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[@id=\"message\"]")).click();
-
-//        var buttons = duo_iframe.findElements(By.xpath("//*[@id='auth_methods']/fieldset/div[1]/input"));
-//        System.out.println(buttons.size());
-    }
-
-
-
-    //public void login() throws InterruptedException {
-//
-//
-//        System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
-//
-//        // browser settings
-//        driver.manage().window().maximize();
-//
-//        // open page
-//        driver.get("https://hud-stage.hudnsc.org/eclass/index.cfm?login=true");
-//        Thread.sleep(500);
-//        WebElement login_btn = driver.findElement(By.id("main_dsp-login-btn"));
-//        Thread.sleep(500);
-//        if (login_btn != null) login_btn.click();
-//
-//        WebElement login_email_field = driver.findElement(By.id("loginEmail"));
-//        Thread.sleep(1000);
-//        login_email_field.sendKeys("freddy.martinez@hudnsc.org");
-//
-//        WebElement login_password_field = driver.findElement(By.id("loginPassword"));
-//        Thread.sleep(1000);
-//        login_password_field.sendKeys("Tara!234");
-//
-//        driver.findElement(By.id("loginSubmit")).click();
-//        // TODO: 1/19/2022 if fields are not null then submit
-//        Thread.sleep(1000);
-//
+////*[@id="navbar"]/ul/li[3]/ul/a[3]/li
 //        var trainees = driver.findElement(By.xpath("//*[@id=\"navbar\"]/ul/li[3]/a"));
 //        Thread.sleep(1000);
 //        if (trainees.getText().length() > 0){
@@ -95,31 +80,42 @@ public class Dashboard {
 //            var container = driver.findElement(By.className("container"));
 //            System.out.println();
 //
-//
-//
-//
-//
-//
-//
-//            // TODO: 1/31/2022 create variable for the table that holds the fields
-//
-//
-//        }
+    }
 
-        // TODO: 1/31/2022 find search link and click
 
-        // FIXME: 1/19/2022 manually authenticate with DUO
-//        driver.close();
+    public void down(){
+        driver.close();
+    }
 
-//        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30)).pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
-//        wait.until(new Function<WebDriver, WebElement>() {
-//            @Override
-//            public WebElement apply(WebDriver webDriver) {
-//                return driver.findElement(By.cssSelector("[id=finish] h4"));
-//            }
-//        });
-//}
+    public boolean handle_multifactor_authentication() throws InterruptedException {
+        // open duo iframe
+        var duo_iframe = driver.findElement(By.id("duo_iframe"));
+        driver.switchTo().frame(duo_iframe);
+        driver.findElement(By.xpath("//*[@id=\"passcode\"]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"message\"]")).click();
+        Thread.sleep(2000);
+        System.out.println("Please provide the  code.  ");
 
+        Scanner sc = new Scanner(System.in);
+        String mfa_code = sc.nextLine();
+//        System.out.println(mfa_code);
+
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#auth_methods > fieldset > div.passcode-label.row-label > div > input")).sendKeys(mfa_code);
+        driver.findElement(By.cssSelector("#passcode")).click();
+
+        return true;
+    }
+
+    public void search() throws InterruptedException {
+        Thread.sleep(500);
+
+        // #keyword
+        driver.findElement(By.cssSelector("#keyword")).sendKeys("pending");
+        Thread.sleep(500);
+
+    }
 }
 
 
--- src/MainActivity.java
+++ src/MainActivity.java
@@ -10,14 +10,20 @@ public class MainActivity {
 
     public static void main(String[] args) throws InterruptedException {
         Dashboard dashboard = new Dashboard(init());
-        dashboard.login();
-        System.out.println(dashboard.driver.getCurrentUrl());
-        System.out.println("successfully signed in ");
-        System.out.println("Multifactor authentication login");
+        if (dashboard.login()){
+            System.out.println("Multifactor authentication login");
+        }
+
+        dashboard.search();
+
+
+
+
+
+
     }
 
     public static WebDriver init(){
-
         System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
