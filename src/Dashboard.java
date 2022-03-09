e778c0586387b1f23f62225a0fa757699c190327
freddy <freddymartinezfm@icloud.com>
2022-03-09 02:05:25 -0600

    successfully switch windows to first pending account check for lender admin button and add those users to a list returned by pending

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,15 +1,9 @@
 import com.sun.security.jgss.GSSUtil;
-import io.opentelemetry.sdk.autoconfigure.spi.ConfigurablePropagatorProvider;
-import net.jodah.failsafe.Fallback;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import javax.swing.*;
-import java.security.cert.TrustAnchor;
-import java.sql.Array;
-import java.sql.Struct;
 import java.util.*;
 
 import static org.openqa.selenium.Keys.ENTER;
@@ -28,51 +22,42 @@ public class Dashboard {
 
     public Dashboard(WebDriver driver){
         this.driver = driver;
-
     }
 
+    public List<String> check_lender_admin(List<String> list) throws InterruptedException {
+        for (int i = 0; i <  list.size(); i++){
+            System.out.print("Checking: \t" + i);
+            System.out.println(list.get(i));
 
-    public Set<String> check_lender_admin() throws InterruptedException {
+            System.out.println("Windows:" + driver.getWindowHandles().size());
+            List<String> new_list = new ArrayList<>();
 
+            Set<String> windows = driver.getWindowHandles();
+            Iterator<String> it = windows.iterator();
+            var parent = it.next();
 
+            while (it.hasNext()){
+                System.out.println("Switching windows ");
+                driver.switchTo().window(it.next());
+                Thread.sleep(5000);
+                var lender_admin_button = driver.findElement(By.cssSelector("#lenderAdminDropdown"));
 
-        Set<String> windows = driver.getWindowHandles();
-        System.out.println("Windows:" + driver.getWindowHandles().size());
-        Iterator<String> it = windows.iterator();
-
-        var parent = it.next();
-
-        while (it.hasNext()){
-            System.out.println("Switching windows ");
-            driver.switchTo().window(it.next());
-            Thread.sleep(5000);
-            System.out.println(driver.findElement(By.cssSelector("#lenderAdminDropdown")));
-
-//            System.out.println(driver.findElement(By.id("lenderAdminDropdown")).isDisplayed());
-//            System.out.println(driver.findElement(By.id("lenderAdminDropdown")).isEnabled());
-
-//            if(driver.findElement(By.id("lenderAdminDropdown")).isDisplayed()){
-//                System.out.println("User has lender admin");
-//                //TODO remove from list of pending accounts
-//
-//                break;
-//            } else {
-//                //TODO proceed with approving account
-//            }
-
-
-
+                if (lender_admin_button.isDisplayed()){
+                    System.out.println("this account has a lender admin, SKIP");
+                    break;
+                } else {
+                    System.out.println("this account does not have a lender admin, DO NOT SKIP");
+                    System.out.println("adding to list");
+                    new_list.add(list.get(i));
+                }
+            }
+            return new_list;
         }
-
-
-        return windows;
+        return null;
     }
 
 
     public boolean login() throws InterruptedException {
-
-//        List<Agent> users =  get_agents();
-
         Agent user = new Agent();
         user.email = "freddy.martinez@hudnsc.org";
         user.password = "Tara!234";
@@ -92,9 +77,7 @@ public class Dashboard {
         login_password_field.sendKeys(user.password);
         driver.findElement(By.id("loginSubmit")).click();
         Thread.sleep(2000);
-
         System.out.println("Successfully enter credentials");
-
         System.out.println("Processing MFA");
         boolean mfa_auth =  handle_multifactor_authentication();
 
@@ -106,7 +89,6 @@ public class Dashboard {
             requestors.click();
             Thread.sleep(1000);
             driver.findElement(By.xpath("//*[@id=\"navbar\"]/ul/li[3]/ul/a[3]/li")).click();
-
         }
         return true;
     }
@@ -139,6 +121,13 @@ public class Dashboard {
     }
 
     public void pending_users() throws InterruptedException {
+        Thread.sleep(500);
+        // #keyword
+        driver.findElement(By.cssSelector("#keyword")).sendKeys("pending");
+        Thread.sleep(500);
+
+        driver.findElement(By.id("traineesSearchSubmit")).click();
+        Thread.sleep(500);
         WebElement table =  driver.findElement(By.xpath("//*[@id=\"trainees_dt\"]"));
         int row_count = table.findElements(By.tagName("tr")).size();
         var rows =  table.findElements(By.tagName("tr"));
@@ -148,7 +137,7 @@ public class Dashboard {
         List<String> pending_account_names = new ArrayList<String>();
         List<WebElement> row_data = null;
 
-        for (int i = 1; i < 3; i++){
+        for (int i = 1; i < col_count; i++){
             row_data =  rows.get(i).findElements(By.tagName("td"));
             String cell_text_value = row_data.get(2).getText().toLowerCase();
             Thread.sleep(1000);
@@ -157,6 +146,7 @@ public class Dashboard {
             pending_account_names.add(cell_text_value);
             System.out.println();
         }
+        System.out.println("All accounts have been opened in new tabs.\t Checking wil now begin...");
 
         var control_click = Keys.chord(Keys.CONTROL, ENTER);
         System.out.println(driver.getTitle());
@@ -164,28 +154,20 @@ public class Dashboard {
         row_data.get(10).findElement(By.tagName("a")).sendKeys(control_click);
         Thread.sleep(500);
         System.out.println();
-        for (var n : pending_account_names){
-            System.out.print("Checking: \t" + n);
-            System.out.println("\t");
+
+        List<String> pending_users =  check_lender_admin(pending_account_names);
+        for (var u : pending_users) {
+            System.out.println(u);
         }
-
-        check_lender_admin();
     }
-
 
     public void search() throws InterruptedException {
         Thread.sleep(500);
-
         // #keyword
         driver.findElement(By.cssSelector("#keyword")).sendKeys("pending");
         Thread.sleep(500);
-
         driver.findElement(By.id("traineesSearchSubmit")).click();
         Thread.sleep(500);
-
-
-
-
     }
 }
 
--- src/MainActivity.java
+++ src/MainActivity.java
@@ -11,13 +11,13 @@ import java.util.Set;
 
 public class MainActivity {
     public static final String STAGE_ENV = "https://hud-stage.hudnsc.org/evars/index.cfm?";
-    public static final String PROD_ENV = "https://hud.hudnsc.org/index.cfm?";
+    public static final String PROD_ENV = "https://hud.hudnsc.org/";
 
 
 
     public static void main(String[] args) throws InterruptedException {
         Dashboard dashboard = new Dashboard(init());
-        dashboard.driver.get(STAGE_ENV);
+        dashboard.driver.get(PROD_ENV);
 
         Thread.sleep(500);
 
@@ -32,7 +32,6 @@ public class MainActivity {
 
         dashboard.search(); // todo should return number of results
         dashboard.pending_users(); //todo should return number of results
-        dashboard.check_lender_admin();
 //        Set<String> real_pending_users =  dashboard.check_lender_admin();
 
     }
