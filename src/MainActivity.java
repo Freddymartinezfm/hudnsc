d3f08f84d41049d8dc4082887e885fb5fae50da7
freddy <freddymartinezfm@icloud.com>
2022-03-09 19:34:47 -0600

    added prod url constant

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,17 +1,11 @@
-import com.sun.security.jgss.GSSUtil;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
-import java.util.Set;
 
 
 
 public class MainActivity {
     public static final String STAGE_ENV = "https://hud-stage.hudnsc.org/evars/index.cfm?";
-    public static final String PROD_ENV = "https://hud.hudnsc.org/index.cfm?";
+    public static final String PROD_ENV = "https://evars.hudnsc.org/index.cfm?login=true&appswitch=switch=dashboard";
 
 
 
@@ -26,7 +20,7 @@ public class MainActivity {
         // browser settings
         ;
         if (dashboard.login()){
-            System.out.println("[+]Multifactor authentication login");
+            System.out.println("[+]Multi-factor authentication login");
         }
 
         dashboard.search(); // todo should return number of results
